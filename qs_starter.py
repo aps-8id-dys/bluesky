c8f729040581e3d51ba74ef6157b3089bc4b66ba
Pete R Jemian <prjemian@users.noreply.github.com>
2023-02-23 09:37:58 -0600

    MNT turn down the exception-reporting noise

--- qs_starter.py
+++ qs_starter.py
@@ -11,6 +11,17 @@ sys.path.append(
 from instrument import iconfig
 from instrument.queueserver import *
 
+# turn down the exception-reporting noise
+_xmode_level - iconfig.get("XMODE_DEBUG_LEVEL")
+if _xmode_level is not None:
+    try:
+        from IPython import get_ipython
+
+        get_ipython().run_line_magic('xmode', _xmode_level)
+        logger.info("xmode exception level: '%s'", _xmode_level)
+    except Exception:
+        pass  # fail silently
+del _xmode_level
 
 print_instrument_configuration()
 print_devices_and_signals()
