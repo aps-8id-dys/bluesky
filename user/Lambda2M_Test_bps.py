c7147ddcb2cf343ec17775a4ee10495c540694b4
Pete R Jemian <prjemian@users.noreply.github.com>
2023-03-02 13:41:12 -0600

    MNT #8 adjust the user plan, as well

--- user/Lambda2M_Test_bps.py
+++ user/Lambda2M_Test_bps.py
@@ -19,16 +19,14 @@ def Rep_Acq(acq_rep=3):
     yield from prepare_count(
         lambda2M.hdf1,
         "Test",
-        0.001,
-        0.001,
-        n_images=10000,
+        0.005, 0.005, n_images=2_000,  # 0.001 might be the shortest
         compression="None",
         auto_save="No",
+        file_template="%s%s_%6.6d.h5",
+        file_path="/home/8ididata/2023-1/bluesky202301",
     )
 
-    lambda2M.hdf1.file_number.put(0)
-    lambda2M.hdf1.file_template.put("%s%s_%6.6d.h5")
-    lambda2M.hdf1.file_path.put("/home/8ididata/2023-1/bluesky202301")
+    yield from bps.mv(lambda2M.hdf1.file_number, 0)
 
     for ii in range(acq_rep):
         print(ii)
