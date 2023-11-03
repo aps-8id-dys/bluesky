6cf1cff24a88e00f83b23680ed3553a59f465535
qzhang234 <qzhang234@anl.gov>
2023-11-03 11:34:37 -0500

    re-arrange

--- instrument/devices/__init__.py
+++ instrument/devices/__init__.py
@@ -4,6 +4,7 @@ local, custom Device definitions
 
 # fmt: off
 
+# ----- ----- ----- ----- -----
 # from ophyd.log import config_ophyd_logging
 # config_ophyd_logging(level="DEBUG")
 #     # 'ophyd' — the logger to which all ophyd log records propagate
@@ -11,14 +12,22 @@ local, custom Device definitions
 #     # 'ophyd.control_layer' — logs requests issued to the underlying control layer (e.g. pyepics, caproto)
 #     # 'ophyd.event_dispatcher' — issues regular summaries of the backlog of updates from the control layer that are being processed on background threads
 
+# ----- ----- ----- ----- -----
+from .aerotech_stages import *
+from .data_management import *
+
+# ----- ----- ----- ----- -----
+# imports that MUST come after the above devices
+from .ad_lambda2M import *
+
+# ----- ----- ----- ----- -----
+# See the .unused_examples folder
+# Copy/move to this folder and edit to use.
 # from .aps_source import *
 # from .aps_undulator import *
 
 # from .area_detector import *
-from .aerotech_stages import *
 # from .calculation_records import *
-from .data_management import *
-
 # from .fourc_diffractometer import *
 # from .ioc_stats import *
 # from .kohzu_monochromator import *
@@ -31,6 +40,3 @@ from .data_management import *
 # from .slits import *
 # from .sixc_diffractometer import *
 # from .temperature_signal import *
-
-# imports that MUST come after the above devices
-from .ad_lambda2M import *
