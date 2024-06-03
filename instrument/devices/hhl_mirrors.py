401a547fc551960a3b22afc5bfad7ce3f701347f
Pete Jemian <prjemian@gmail.com>
2024-06-03 14:26:21 -0500

    ENH #15 add HHL Slit devices: mr1 & mr2

---
+++ instrument/devices/hhl_mirrors.py
@@ -0,0 +1,39 @@
+"""
+High-heat load mirrors in station 8-ID-A.
+"""
+
+__all__ = """
+    mr1
+    mr2
+""".split()
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.info(__file__)
+
+from ophyd import Component, Device, EpicsMotor
+
+
+class HHL_Mirror1(Device):
+
+    x = Component(EpicsMotor, "FMBO:m4")
+    y = Component(EpicsMotor, "FMBO:m2")
+    coarse_pitch = Component(EpicsMotor, "FMBO:m6")
+    fine_pitch = Component(EpicsMotor, "FMBO:Piezo:m1")
+    flag = Component(EpicsMotor, "CR8-A1:m5")
+
+
+class HHL_Mirror2(Device):
+
+    x = Component(EpicsMotor, "FMBO:m3")
+    y = Component(EpicsMotor, "FMBO:m1")
+    coarse_pitch = Component(EpicsMotor, "FMBO:m5")
+    fine_pitch = Component(EpicsMotor, "FMBO:Piezo:m2")
+    flag = Component(EpicsMotor, "CR8-A1:m6")
+    bender1 = Component(EpicsMotor, "FMBO:m7")
+    bender1 = Component(EpicsMotor, "FMBO:m8")
+
+
+mr1 = HHL_Mirror1("8idaSoft:", name="mr1")
+mr2 = HHL_Mirror2("8idaSoft:", name="mr2")
