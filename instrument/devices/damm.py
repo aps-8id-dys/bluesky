1c3502ed570df9650d84d01f891a7c90fb9bd8bb
Eric Codrea <codrea.eric@gmail.com>
2024-06-06 09:53:50 -0500

    formatting

--- instrument/devices/damm.py
+++ instrument/devices/damm.py
@@ -1,4 +1,3 @@
-
 """
 HHL Slits in station 8-ID-A
 """
@@ -12,8 +11,6 @@ import logging
 logger = logging.getLogger(__name__)
 logger.info(__file__)
 
-# from apstools.devices.hhl_slits import HHLSlits
-
 from ophyd import Component as Cpt
 from ophyd import Device
 from ophyd import FormattedComponent as FCpt
@@ -21,7 +18,6 @@ from ophyd import EpicsMotor
 
 
 class Slit2(Device):
-
     def __init__(
         self,
         prefix: str,
@@ -42,5 +38,6 @@ class Slit2(Device):
     # Real motors that directly control the slits
     pitch = FCpt(EpicsMotor, "{motor_prefix}:{_h_motor}", labels={"motors"})
     yaw = FCpt(EpicsMotor, "{motor_prefix}:{_v_motor}", labels={"motors"})
+
 
 damm = Slit2(name="damm", prefix="8iddSoft:CR8-D1:US", h_motor="m2", v_motor="m3")
