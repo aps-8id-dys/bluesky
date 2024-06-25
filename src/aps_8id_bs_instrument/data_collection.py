33d6179c6034a10e194533e7b18718dc97ff8abd
Eric Codrea <codrea.eric@gmail.com>
2024-06-25 12:39:59 -0500

    first changes to make it package importable

--- src/aps_8id_bs_instrument/data_collection.py
+++ src/aps_8id_bs_instrument/data_collection.py
@@ -6,33 +6,17 @@ import os
 
 from apstools.utils import *  # noqa
 
-from . import iconfig
-from .callbacks.spec_data_file_writer import specwriter
 from .devices import *  # noqa
-from .initialize import *  # noqa
 from .plans import *  # noqa
 
 ## ipython helpers
-from .utils.ipy_helper import *  # noqa
 from .utils.mpl_helper import *  # noqa
 from .utils.session_logs import logger
 
+# from .initialize import *  # noqa
+
 logger.info(__file__)
 
-# conda environment name
-_conda_prefix = os.environ.get("CONDA_PREFIX")
-if _conda_prefix is not None:
-    logger.info("CONDA_PREFIX = %s", _conda_prefix)
-del _conda_prefix
 
 
-if iconfig.get("WRITE_SPEC_DATA_FILES", False):
-    if specwriter is not None:
-        RE.subscribe(specwriter.receiver)  # noqa
-        logger.info(f"writing to SPEC file: {specwriter.spec_filename}")
-        logger.info("   >>>>   Using default SPEC file name   <<<<")
-        logger.info("   file will be created when bluesky ends its next scan")
-        logger.info("   to change SPEC file, use command:   newSpecFile('title')")
-
-
-logger.info("#### Startup is complete. ####")
+logger.info("#### data collection tools are loaded. ####")
