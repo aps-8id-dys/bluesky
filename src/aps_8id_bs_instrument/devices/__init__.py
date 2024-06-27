9d0585ca019f3f9819f1e3ee4bb5731dfd284c1a
Rafael Vescovi <ravescovi@gmail.com>
2024-06-27 10:11:37 -0500

    fixes / cleanup

--- src/aps_8id_bs_instrument/devices/__init__.py
+++ src/aps_8id_bs_instrument/devices/__init__.py
@@ -5,38 +5,32 @@ local, custom Device definitions
 # flake8: noqa
 
 try:
-    from .aerotech_stages import (
-        AerotechDetectorStage,
-        AerotechRheometerStage,
-        AerotechSampleStage,
-    )
+    from .aerotech_stages import sample, detector, rheometer
 except Exception as excuse:
     print(f"Could not import Aerotech: {excuse}")
 
 try:
     from .flight_tube import (
-        FlightTubeBeamStop,
-        FlightTubeDetector,
+        det,
+        bs,
     )
 except Exception as excuse:
     print(f"Could not import Flight Tube: {excuse}")
 
-# from .. import load_config
 from .ad_common import EigerDetectorCam_V34
-from .damm import damm
 from .data_management import (
     DM_WorkflowConnector,
     dm_experiment,
 )
+
+from .damm import damm
 from .flag_4 import fl4
-from .hhl_mirrors import HHL_Mirror1, HHL_Mirror2
+from .hhl_mirrors import mr1, mr2
 from .hhl_slits import wb_slit
-from .idt_mono import IDTMono
-from .meascomp_usb_ctr import MeasCompCtr
-from .qnw_device import QnwDevice
+from .idt_mono import idt_mono
+from .meascomp_usb_ctr import mcs
+from .qnw_device import qnw_env1, qnw_env2, qnw_env3
 from .slit_4 import sl4
-
-
 from .ad_eiger_4M import eiger4M
 from .ad_flag1 import flag1ad
 from .ad_flag2 import flag2ad
