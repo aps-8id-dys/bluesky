964c21e31894ef7632df09f8a15ca3c6a9a25405
Eric Codrea <codrea.eric@gmail.com>
2024-07-10 12:39:47 -0500

    trying some stuff

--- scripts/start_ipython.sh
+++ scripts/start_ipython.sh
@@ -3,4 +3,4 @@
 #!/bin/bash
 
 # Check if the initialize.py file exists
-ipython -i -c "from aps_8id_bs_instrument.initialize import initialize; initialize()"
+ipython -i -c "from aps_8id_bs_instrument import ipython_startup"
