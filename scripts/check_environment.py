b27ac25d71c194d00d389c4a525373707cd7320b
Eric Codrea <codrea.eric@gmail.com>
2024-07-12 11:40:26 -0500

    got rid of old files

--- scripts/check_environment.py
+++ scripts/check_environment.py
@@ -5,9 +5,5 @@ configure the Bluesky framework
 """
 
 from aps_8id_bs_instrument.tests.check_subnet import warn_if_not_aps_controls_subnet
-from aps_8id_bs_instrument.utils.metadata import metadata
 
 warn_if_not_aps_controls_subnet()
-print(metadata())
-
-# TODO: More of a warning than anything
