bf40d3177f0a34622e341072360c94158c7bb306
Eric Codrea <codrea.eric@gmail.com>
2024-08-19 15:07:35 -0500

    fix to run from  any dir

--- scripts/bs_ipy_starter.sh
+++ scripts/bs_ipy_starter.sh
@@ -4,4 +4,7 @@ echo "You chose ipython!"
 # Standard BCDA setup at APS defines this pointing to APSshare.
 # We choose here to get PyEpics from the conda environment.
 unset PYEPICS_LIBCA # TODO: Need to fix this more permenantly
-ipython -i -c "%run bs_ipy_profile.ipy"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+ipython -i -c "%run $SCRIPT_DIR/bs_ipy_profile.ipy"
