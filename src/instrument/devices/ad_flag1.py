f59c0feb0d6a12305dfb0a5e8e644aa81f23bd11
Pete R Jemian <prjemian@users.noreply.github.com>
2024-08-20 13:50:26 -0500

    DOC #50 per review

--- src/instrument/devices/ad_flag1.py
+++ src/instrument/devices/ad_flag1.py
@@ -15,5 +15,8 @@ logger = logging.getLogger(__name__)
 logger.info(__file__)
 
 flag1ad = XpcsAreaDetectorFactory(
-    "FLAG2", use_process=False, use_roi=False, use_stats=False
+    "FLAG2", 
+    use_process=False,  # Do not use the AD Processing plugin: PROC1
+    use_roi=False,   # Do not use the AD Region-of-Interest plugin: ROI1
+    use_stats=False,  # Do not use the AD Statistics plugin: Stats1
 )
