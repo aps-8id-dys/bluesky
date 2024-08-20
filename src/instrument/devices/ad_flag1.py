86c4554696b6553a94ed671091f183d363bab1d2
Pete R Jemian <prjemian@users.noreply.github.com>
2024-08-20 13:54:19 -0500

    STY #50

--- src/instrument/devices/ad_flag1.py
+++ src/instrument/devices/ad_flag1.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 logger.info(__file__)
 
 flag1ad = XpcsAreaDetectorFactory(
-    "FLAG2", 
+    "FLAG2",
     use_process=False,  # Do not use the AD Processing plugin: PROC1
     use_roi=False,  # Do not use the AD Region-of-Interest plugin: ROI1
     use_stats=False,  # Do not use the AD Statistics plugin: Stats1
