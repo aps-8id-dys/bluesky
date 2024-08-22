5e89153ce680a10d4284722a2ec64648ef51e787
Eric Codrea <codrea.eric@gmail.com>
2024-08-22 16:46:19 -0500

    added 8 id slit 4

--- src/instrument/devices/slit_4.py
+++ src/instrument/devices/slit_4.py
@@ -8,57 +8,32 @@ __all__ = """
 
 import logging
 
-from ophyd import Device
-from ophyd import EpicsMotor
-from ophyd import FormattedComponent as FCpt
+from apstools.devices.positioner_soft_done import PVPositionerSoftDone
+from apstools.synApps.db_2slit import Optics2Slit1D
+from apstools.synApps.db_2slit import Optics2Slit2D_HV
+from ophyd import Component as cpt
+from ophyd import EpicsSignal
 
 logger = logging.getLogger(__name__)
 logger.info(__file__)
 
 
-class Slit6(Device):
-    def __init__(
-        self,
-        prefix: str,
-        v_neg_motor: str,
-        v_pos_motor: str,
-        h_neg_motor: str,
-        h_pos_motor: str,
-        h_base_motor: str,
-        v_base_motor: str,
-        *args,
-        **kwargs,
-    ):
-        # Determine the prefix for the motors
-        pieces = prefix.strip(":").split(":")
-        self.motor_prefix = ":".join(pieces[:-1])
-
-        self._v_neg_motor = v_neg_motor
-        self._v_pos_motor = v_pos_motor
-
-        self._h_neg_motor = h_neg_motor
-        self._h_pos_motor = h_pos_motor
-
-        self._h_base_motor = h_base_motor
-        self._v_base_motor = v_base_motor
-
-        super().__init__(prefix, *args, **kwargs)
-
-    v_neg = FCpt(EpicsMotor, "{motor_prefix}:{_v_neg_motor}", labels={"motors"})
-    v_pos = FCpt(EpicsMotor, "{motor_prefix}:{_v_pos_motor}", labels={"motors"})
-    h_neg = FCpt(EpicsMotor, "{motor_prefix}:{_h_neg_motor}", labels={"motors"})
-    h_pos = FCpt(EpicsMotor, "{motor_prefix}:{_h_pos_motor}", labels={"motors"})
-    h_base = FCpt(EpicsMotor, "{motor_prefix}:{_h_base_motor}", labels={"motors"})
-    v_base = FCpt(EpicsMotor, "{motor_prefix}:{_v_base_motor}", labels={"motors"})
+class ID8Optics2Slit1D(Optics2Slit1D):
+    xn = cpt(PVPositionerSoftDone, "", setpoint_pv="xn", readback_pv="xn.RBV")
+    xp = cpt(PVPositionerSoftDone, "", setpoint_pv="xp", readback_pv="xp.RBV")
+    size = cpt(PVPositionerSoftDone, "", setpoint_pv="size", readback_pv="size.RBV")
+    center = cpt(
+        PVPositionerSoftDone, "", setpoint_pv="center", readback_pv="center.RBV"
+    )
+    sync = cpt(
+        EpicsSignal, "doSync", put_complete=True, kind="omitted"
+    )  # TODO: What is the pv value for sync
 
 
-sl4 = Slit6(
-    name="sl4",
-    prefix="8iddSoft:CR8-D1:US",
-    v_neg_motor="m11",
-    v_pos_motor="m12",
-    h_neg_motor="m13",
-    h_pos_motor="m14",
-    h_base_motor="m15",
-    v_base_motor="m16",
-)
+class ID8Optics2Slit2D_HV(Optics2Slit2D_HV):
+    h = cpt(ID8Optics2Slit1D, "H")
+    v = cpt(ID8Optics2Slit1D, "V")
+
+
+# Create the sl9 object
+sl4 = ID8Optics2Slit2D_HV("8iddSoft:Slit1", name="sl-4")
