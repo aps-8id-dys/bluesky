fa48661e666e94b8cf722bffe788549a3476b880
Eric Codrea <codrea.eric@gmail.com>
2024-08-22 16:10:09 -0500

    sl9 pre-commited

--- src/instrument/devices/slit_9.py
+++ src/instrument/devices/slit_9.py
@@ -17,16 +17,23 @@ from ophyd import EpicsSignal
 logger = logging.getLogger(__name__)
 logger.info(__file__)
 
+
 class CustomOptics2Slit1D(Optics2Slit1D):
     xn = cpt(PVPositionerSoftDone, "", setpoint_pv="xn", readback_pv="xn.RBV")
     xp = cpt(PVPositionerSoftDone, "", setpoint_pv="xp", readback_pv="xp.RBV")
     size = cpt(PVPositionerSoftDone, "", setpoint_pv="size", readback_pv="size.RBV")
-    center = cpt(PVPositionerSoftDone, "", setpoint_pv="center", readback_pv="center.RBV")
-    sync = cpt(EpicsSignal, "doSync", put_complete=True, kind="omitted") #TODO: What is the pv value for sync
+    center = cpt(
+        PVPositionerSoftDone, "", setpoint_pv="center", readback_pv="center.RBV"
+    )
+    sync = cpt(
+        EpicsSignal, "doSync", put_complete=True, kind="omitted"
+    )  # TODO: What is the pv value for sync
+
 
 class CustomOptics2Slit2D_HV(Optics2Slit2D_HV):
     h = cpt(CustomOptics2Slit1D, "H")
     v = cpt(CustomOptics2Slit1D, "V")
+
 
 # Create the sl9 object
 sl9 = CustomOptics2Slit2D_HV("8idiSoft:Slit2", name="sl-9")
