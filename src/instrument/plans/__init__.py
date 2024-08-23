409c28811aeb290eecd22a2057c82e69bf77ee9c
Eric Codrea <codrea.eric@gmail.com>
2024-08-22 23:22:06 -0500

    added qz plan to main

--- src/instrument/plans/__init__.py
+++ src/instrument/plans/__init__.py
@@ -11,3 +11,4 @@ from .demo_sim_1d import demo_sim_1d
 ## beamline specific plans
 # from .bdp_demo import xpcs_bdp_demo_plan
 from .mesh_plans import xpcs_mesh
+from .select_sample_env import select_sample_env
---
+++ src/instrument/plans/select_sample_env.py
@@ -0,0 +1,26 @@
+__all__ = """
+    select_sample_env
+""".split()
+
+import bluesky.plan_stubs as bps
+from ..devices import granite, sample
+
+def select_sample_env(env: str):
+    # choices = {
+    #     "qnw": 933.0,
+    #     "rheometer": 62,
+    # }
+    # target = choices.get(env)
+    # if target is None:
+    #     raise KeyError(f"Unkown environment {env=!r}")
+
+    if env == "qnw":
+        yield from bps.mv(granite.x, 933.0)
+        yield from bps.mv(sample.x, 150)
+    if env == "rheometer":
+        yield from bps.mv(granite.x, 62)
+        yield from bps.mv(sample.x, 0.5)
+    elif env == "robot":
+        yield from bps.mv(granite.x, 62)
+        yield from bps.mv(sample.x, 298)
+
