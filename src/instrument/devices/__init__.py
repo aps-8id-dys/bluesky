5b262c7f44902e53dea6a2eb4f88b627301ca67b
MDecarabas <72196105+MDecarabas@users.noreply.github.com>
2024-08-26 13:53:07 -0500

    Added bd5a device and changed flag device name
    * added bd5a device
    
    * changed name from fl4 to flag4

--- src/instrument/devices/__init__.py
+++ src/instrument/devices/__init__.py
@@ -23,9 +23,10 @@ except Exception as excuse:
 
 
 ## Beamline specific devices
+from .bd_5a import bd5a
 from .damm import damm
+from .flag_4 import flag4
 from .fl import fl2, fl3
-from .flag_4 import fl4
 from .hhl_mirrors import mr1, mr2
 from .hhl_slits import wb_slit, mono_slit
 from .idt_mono import idt_mono
