6ab20a789b1cb915ba7e28a6fe933d0fbd31a8ea
qzhang234 <qzhang234@anl.gov>
2024-08-30 14:33:32 -0500

    ENH #64 add bd6a (motors), update xbpm support

--- src/instrument/devices/xbpm2_in_8ide.py
+++ src/instrument/devices/xbpm2_in_8ide.py
@@ -3,37 +3,49 @@ XBPM2: X-ray beam position monitor in 8-ID-E for use in I station
 """
 
 __all__ = """
+    bd6a
     xbpm2
 """.split()
 
 import logging
 
 from ophyd import Component
+from ophyd import EpicsMotor
+from ophyd import EpicsSignalRO
+from ophyd import MotorBundle
 from ophyd import QuadEM
-from ophyd.areadetector import ImagePlugin
-from ophyd.areadetector import StatsPlugin
+from ophyd.areadetector.plugins import ImagePlugin_V34
+from ophyd.areadetector.plugins import StatsPlugin_V34
+from ophyd.quadem import QuadEMPort
 
 logger = logging.getLogger(__name__)
 logger.info(__file__)
 
 
-class MyImagePlugin(ImagePlugin):
-    pool_max_buffers = None
+class BD6AMotors(MotorBundle):
+    """Base motors for XBPM2."""
 
-
-class MyStatsPlugin(StatsPlugin):
-    pool_max_buffers = None
+    h = Component(EpicsMotor, "m9", labels={"motor"})
+    v = Component(EpicsMotor, "m10")
 
 
 class MyXBPM2(QuadEM):
-    """Only change the AD plugins which don't have pool_max_buffers."""
+    """Revisions to ophyd class."""
 
-    current1 = Component(MyStatsPlugin, "Current1:")
-    current2 = Component(MyStatsPlugin, "Current2:")
-    current3 = Component(MyStatsPlugin, "Current3:")
-    current4 = Component(MyStatsPlugin, "Current4:")
-    image = Component(MyImagePlugin, "image1:")
-    sum_all = Component(MyStatsPlugin, "SumAll:")
+    conf = Component(QuadEMPort, port_name="T4U_BPM")
+
+    # latest versions of the AD plugins
+    current1 = Component(StatsPlugin_V34, "Current1:")
+    current2 = Component(StatsPlugin_V34, "Current2:")
+    current3 = Component(StatsPlugin_V34, "Current3:")
+    current4 = Component(StatsPlugin_V34, "Current4:")
+    image = Component(ImagePlugin_V34, "image1:")
+    sum_all = Component(StatsPlugin_V34, "SumAll:")
+
+    # better as text than numbers
+    firmware = Component(EpicsSignalRO, "Firmware", string=True, kind="config")
+    model = Component(EpicsSignalRO, "Model", string=True, kind="config")
 
 
 xbpm2 = MyXBPM2("8idiSoft:T4U_BPM:", name="xbpm2")
+bd6a = BD6AMotors("8ideSoft:CR8-E2:", name="bd6a")
