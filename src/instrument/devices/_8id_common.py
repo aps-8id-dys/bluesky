26c57c9d8d73831e450abcdb85750dc1dd89c4c5
Pete R Jemian <prjemian@users.noreply.github.com>
2024-09-02 10:11:02 -0500

    MNT #64 generalize Transfocator

--- src/instrument/devices/_8id_common.py
+++ src/instrument/devices/_8id_common.py
@@ -7,6 +7,7 @@ Classes used by various devices at 8-ID.
 __all__ = """
     HV_Motors
     SydorTP4U
+    Transfocator
 """.split()
 
 
@@ -36,39 +37,39 @@ class HV_Motors(MotorBundle):
         Includes trailing delimiter, such as ':', as needed.
         (Same convention as used with GUI screens, IOC startup scripts,
         and other EPICS configuration.)
-    h_pv
+    pv_h
         (str) : EPICS PV suffix for the horizontal motor PV.
-    v_pv
+    pv_v
         (str) : EPICS PV suffix for the vertical motor PV.
 
     Example::
 
-        base = HV_Motors("IOC:DB:", name="base", h_pv="m9", v_pv="m10")
+        base = HV_Motors("IOC:DB:", name="base", pv_h="m9", pv_v="m10")
         # which creates:
         # base.h.pvname: IOC:DB:m9
         # base.v.pvname: IOC:DB:m10
     """
 
-    h = FCpt(EpicsMotor, "{prefix}{_h_pv}", labels={"motors"})
-    v = FCpt(EpicsMotor, "{prefix}{_v_pv}", labels={"motors"})
+    h = FCpt(EpicsMotor, "{prefix}{_pv_h}", labels={"motors"})
+    v = FCpt(EpicsMotor, "{prefix}{_pv_v}", labels={"motors"})
 
     def __init__(
         self,
         prefix: str,
         *args,
-        h_pv: str = "",
-        v_pv: str = "",
+        pv_h: str = "",
+        pv_v: str = "",
         **kwargs,
     ):
-        self._h_pv = h_pv
-        self._v_pv = v_pv
+        self._pv_h = pv_h
+        self._pv_v = pv_v
         super().__init__(prefix, *args, **kwargs)
 
 
 class SydorTP4U(QuadEM):
     """
     Sydor TP4U quad electrometer.
-    
+
     Example::
 
         xbpm2 = SydorTP4U("8idiSoft:T4U_BPM:", name="xbpm2")
@@ -87,3 +88,91 @@ class SydorTP4U(QuadEM):
     # better as text than numbers
     firmware = Component(EpicsSignalRO, "Firmware", string=True, kind="config")
     model = Component(EpicsSignalRO, "Model", string=True, kind="config")
+
+
+class Transfocator(MotorBundle):
+    """
+    Transfocator (compound refractive lens, CRL).
+
+    .. note:: POLAR instrument has some related functions (`transfocator.py`).
+        https://github.com/APS-4ID-POLAR/polar_instrument/pull/7/files
+
+    Example::
+
+        rl1 = Transfocator(
+            "8iddSoft:TRANS:",
+            name="rl1",
+            pv_y="m1",
+            pv_x="m2",
+            pv_yaw="m3",
+            pv_pitch="m4",
+            pv_lens1="m5",
+            pv_lens2="m6",
+            pv_lens3="m7",
+            pv_lens4="m8",
+            pv_lens5="m9",
+            pv_lens6="m10",
+            pv_lens7="m11",
+            pv_lens8="m12",
+            pv_lens9="m13",
+            pv_lens10="m14",
+        )
+    """
+    # motors that position the CRL assembly
+    # By some convention, the names x&y are used rather than h&v.
+    x = FCpt(EpicsMotor, "{prefix}{_pv_x}", labels={"motors"})
+    y = FCpt(EpicsMotor, "{prefix}{_pv_y}", labels={"motors"})
+    pitch = FCpt(EpicsMotor, "{prefix}{_pv_pitch}", labels={"motors"})
+    yaw = FCpt(EpicsMotor, "{prefix}{_pv_yaw}", labels={"motors"})
+
+    # motors that insert or remove each lens
+    # Note: Operated as two-position motors.  Consider refactoring as such.
+    lens1 = FCpt(EpicsMotor, "{prefix}{_pv_lens1}", labels={"lens"})
+    lens2 = FCpt(EpicsMotor, "{prefix}{_pv_lens2}", labels={"lens"})
+    lens3 = FCpt(EpicsMotor, "{prefix}{_pv_lens3}", labels={"lens"})
+    lens4 = FCpt(EpicsMotor, "{prefix}{_pv_lens4}", labels={"lens"})
+    lens5 = FCpt(EpicsMotor, "{prefix}{_pv_lens5}", labels={"lens"})
+    lens6 = FCpt(EpicsMotor, "{prefix}{_pv_lens6}", labels={"lens"})
+    lens7 = FCpt(EpicsMotor, "{prefix}{_pv_lens7}", labels={"lens"})
+    lens8 = FCpt(EpicsMotor, "{prefix}{_pv_lens8}", labels={"lens"})
+    lens9 = FCpt(EpicsMotor, "{prefix}{_pv_lens9}", labels={"lens"})
+    lens10 = FCpt(EpicsMotor, "{prefix}{_pv_lens10}", labels={"lens"})
+
+    # TODO: Describe magnification of each lens.
+    # TODO: Add method(s) to select & report magnification based on lens combination.
+
+    def __init__(
+        self,
+        prefix: str,
+        *args,
+        pv_x: str = "",
+        pv_y: str = "",
+        pv_yaw: str = "",
+        pv_pitch: str = "",
+        lens1: str = "",
+        lens2: str = "",
+        lens3: str = "",
+        lens4: str = "",
+        lens5: str = "",
+        lens6: str = "",
+        lens7: str = "",
+        lens8: str = "",
+        lens9: str = "",
+        lens10: str = "",
+        **kwargs,
+    ):
+        self._pv_x = pv_x
+        self._pv_y = pv_y
+        self._pv_pitch = pv_pitch
+        self._pv_yaw = pv_yaw
+        self._pv_lens0 = pv_lens1
+        self._pv_lens2 = pv_lens2
+        self._pv_lens3 = pv_lens3
+        self._pv_lens4 = pv_lens4
+        self._pv_lens5 = pv_lens5
+        self._pv_lens6 = pv_lens6
+        self._pv_lens7 = pv_lens7
+        self._pv_lens8 = pv_lens8
+        self._pv_lens9 = pv_lens9
+        self._pv_lens10 = pv_lens10
+        super().__init__(prefix, *args, **kwargs)
--- src/instrument/devices/xbpm2_in_8ide.py
+++ src/instrument/devices/xbpm2_in_8ide.py
@@ -16,5 +16,5 @@ from ._8id_common import HV_Motors, QuadEM_SydorTP4U
 logger = logging.getLogger(__name__)
 logger.info(__file__)
 
-bd6a = HV_Motors("8iddSoft:CR8-E2:", name="bd6a", h_pv="m9", v_pv="m10")
+bd6a = HV_Motors("8iddSoft:CR8-E2:", name="bd6a", pv_h="m9", pv_v="m10")
 xbpm2 = SydorTP4U("8idiSoft:T4U_BPM:", name="xbpm2")
