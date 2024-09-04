4ff937cef0f1ec2ab0697d70970ed83c3d737214
qzhang234 <qzhang234@anl.gov>
2024-09-04 16:25:00 -0500

    MNT #52 wrap with try..except

--- src/instrument/devices/ad_flag2.py
+++ src/instrument/devices/ad_flag2.py
@@ -14,9 +14,14 @@ from .ad_common import XpcsAreaDetectorFactory
 logger = logging.getLogger(__name__)
 logger.info(__file__)
 
-flag2ad = XpcsAreaDetectorFactory(
-    "FLAG2",
-    use_process=False,  # Do not use the AD Processing plugin: PROC1
-    use_roi=False,  # Do not use the AD Region-of-Interest plugin: ROI1
-    use_stats=False,  # Do not use the AD Statistics plugin: Stats1
-)
+try:
+    flag2ad = XpcsAreaDetectorFactory(
+        "FLAG2",
+        use_process=False,  # Do not use the AD Processing plugin: PROC1
+        use_roi=False,  # Do not use the AD Region-of-Interest plugin: ROI1
+        use_stats=False,  # Do not use the AD Statistics plugin: Stats1
+    )
+    flag2ad.wait_for_connection()
+except Exception as cause:
+    logger.warning(f"Could not create flag2ad: {cause}")
+    flag2ad = None
