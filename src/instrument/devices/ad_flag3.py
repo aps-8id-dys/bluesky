c484db7e61ccdd6d8e37424b0fd39de6cd89678b
qzhang234 <qzhang234@anl.gov>
2024-09-27 17:25:47 -0500

    ENH #79 add flag3ad and flag4ad

---
+++ src/instrument/devices/ad_flag3.py
@@ -0,0 +1,22 @@
+"""
+EPICS area_detectors
+"""
+
+__all__ = """
+    flag3ad
+""".split()
+
+
+import logging
+
+from .ad_common import XpcsAreaDetectorFactory
+
+logger = logging.getLogger(__name__)
+logger.info(__file__)
+
+flag3ad = XpcsAreaDetectorFactory(
+    "FLAG3",
+    use_process=False,  # Do not use the AD Processing plugin: PROC1
+    use_roi=False,  # Do not use the AD Region-of-Interest plugin: ROI1
+    use_stats=False,  # Do not use the AD Statistics plugin: Stats1
+)
