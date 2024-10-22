633b4e402125dee1613fb687db348eb5bf7d158a
Pete R Jemian <prjemian@gmail.com>
2024-10-22 16:40:35 -0500

    MNT #95

--- src/instrument/plans/independent.py
+++ src/instrument/plans/independent.py
@@ -5,13 +5,17 @@ Independent plans.
     ~simple_acquire
 """
 
-from bluesky import plans as bp
+import pathlib
+
+from apstools.devices import DM_WorkflowConnector
+from apstools.utils import share_bluesky_metadata_with_dm
 from bluesky import plan_stubs as bps
+from bluesky import plans as bp
 from bluesky import preprocessors as bpp
 
-from .ad_eiger_4M import eiger4M
 from ..callbacks.nexus_data_file_writer import nxwriter
-from ..initialize_bs_tools import oregistry
+from ..initialize_bs_tools import cat
+from .ad_eiger_4M import eiger4M
 
 EMPTY_DICT = {}
 
@@ -43,9 +47,60 @@ def setup_detector(det, acq_time, num_frames, file_name):
     yield from bps.mv(det.cam.number_of_frames, num_frames)
     yield from bps.mv(det.hdf1.file_name, file_name)
 
+
+def kickoff_dm_workflow(experiment_name, file_name, qmap_file, uid):
+    """Start a DM workflow for this bluesky run."""
+    workflow_name = "xpcs8-02-gladier-boost"
+    run = cat[uid]
+    dm_workflow = DM_WorkflowConnector(name="dm_workflow")
+    yield from bps.mv(dm_workflow.concise_reporting, True)
+    yield from bps.mv(dm_workflow.reporting_period, 10)  # seconds between updates
+
+    # TODO: How to turn off all reporting about the workflow?
+    yield from dm_workflow.run_as_plan(
+        workflow=workflow_name,
+        wait=False,
+        timeout=999_999_999_999,
+        # all kwargs after this line are DM argsDict content
+        filePath=file_name,
+        experimentName=experiment_name,
+        qmap=qmap_file,
+        # from the plan's API
+        # smooth=wf_smooth,
+        # gpuID=wf_gpuID,
+        # beginFrame=wf_beginFrame,
+        # endFrame=wf_endFrame,
+        # strideFrame=wf_strideFrame,
+        # avgFrame=wf_avgFrame,
+        # type=wf_type,
+        # dq=wf_dq,
+        # verbose=wf_verbose,
+        # saveG2=wf_saveG2,
+        # overwrite=wf_overwrite,
+        # analysisMachine=analysisMachine,
+    )
+
+    # upload bluesky run metadata to APS DM
+    share_bluesky_metadata_with_dm(experiment_name, workflow_name, run)
+
+    # TODO: print the workflow process id.
+
+
 def full_acquisition():
     """These are the data acquisition steps for a user."""
     det = eiger4M
     yield from setup_detector(det, 0.1, 1000, "A001_001")
-    yield from simple_acquire(det)
-    yield from nxwriter.wait_writer_plan_stub()
+
+    uids = yield from simple_acquire(det)
+    print(f"Bluesky run: {uids=}")
+
+    try:
+        yield from nxwriter.wait_writer_plan_stub()
+        yield from kickoff_dm_workflow(
+            "my_dm_experiment",
+            pathlib.Path(det.hdf1.full_file_name.get()).name,
+            "my_qmap_file.h5",
+            uids[0],
+        )
+    except Exception as exc:
+        print(f"Exception: {exc}")
