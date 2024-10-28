af29181ecfceb9e0cbb0caad8e241dd970619887
qzhang234 <qzhang234@anl.gov>
2024-10-28 15:26:00 -0500

    STY #95

--- src/instrument/devices/labjack_support.py
+++ src/instrument/devices/labjack_support.py
@@ -2,8 +2,10 @@
 
 from ophyd import Component, Device, EpicsSignal
 
+
 class LabJack(Device):
     operation = Component(EpicsSignal, "Bo0")
     logic = Component(EpicsSignal, "Bo1")
+
 
 labjack = LabJack("8idiSoft:LJT705:", name="labjack")
