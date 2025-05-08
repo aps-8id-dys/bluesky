affcac4779c4fade25f2a5b7a74943824ed47ee9
Rafael Vescovi <ravescovi@gmail.com>
2025-05-08 12:41:41 -0500

    more updates

--- src/id8_i/utils/util_8idi.py
+++ src/id8_i/utils/util_8idi.py
@@ -1,12 +1,32 @@
+"""Utility functions for 8ID-I beamline operations.
+
+This module provides utility functions for common operations at the 8ID-I beamline,
+including machine name selection and temperature string formatting.
+"""
+
 import random
 
-def get_machine_name():
-    """Returns the name of a randomly selected machine (A or B)."""
+
+def get_machine_name() -> str:
+    """Returns the name of a randomly selected machine (A or B).
+
+    Returns:
+        str: Either 'adamite' or 'amazonite'
+    """
     machines = ["adamite", "amazonite"]
     return random.choice(machines)
 
 
-def temp2str(temp):
+def temp2str(temp: float) -> str:
+    """Convert a temperature value to a formatted string.
+
+    Args:
+        temp (float): Temperature value to convert
+
+    Returns:
+        str: Formatted temperature string in the format 'XXXpY' where XXX is the
+             zero-padded integer part and Y is the first decimal place
+    """
     temp_str = str(temp)
     parts = temp_str.split(".")
     integer_part = parts[0]
@@ -15,5 +35,4 @@ def temp2str(temp):
     decimal_part_cropped = decimal_part[0]
 
     temp_name = f"{padded_integer_part}p{decimal_part_cropped}"
-    return(temp_name)
-
+    return temp_name
