b6037d497a114b69f2737128b7899098f0f1030c
Eric Codrea <codrea.eric@gmail.com>
2025-05-09 12:39:27 -0500

    removed instance

--- src/id8_i/devices/flight_path.py
+++ src/id8_i/devices/flight_path.py
@@ -5,6 +5,3 @@ from ophyd import EpicsMotor
 
 class FlightPath(Device):
     length = Component(EpicsMotor, "m1", name="length")
-
-
-flight_path_8idi = FlightPath("8idiSoft:FLIGHT:", name="flight_path_8idi")
